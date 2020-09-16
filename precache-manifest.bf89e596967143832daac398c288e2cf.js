f3418dcd17ad5437c19ead66e48c1e47dac10dd4
villalva03 <villalva03@gmail.com>
2020-09-16 18:36:45 -0300

    Updates

---
+++ precache-manifest.bf89e596967143832daac398c288e2cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfe6d3a048f4877d729d21173d11d0e7",
+    "url": "/404NotFound/index.html"
+  },
+  {
+    "revision": "7f315989f482c7e450c9",
+    "url": "/404NotFound/static/css/main.b11962fe.chunk.css"
+  },
+  {
+    "revision": "51a3f68657eb19ae3581",
+    "url": "/404NotFound/static/js/2.b9955f1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/404NotFound/static/js/2.b9955f1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f315989f482c7e450c9",
+    "url": "/404NotFound/static/js/main.c4a51245.chunk.js"
+  },
+  {
+    "revision": "763c5886e50545964e02",
+    "url": "/404NotFound/static/js/runtime-main.76375cf2.js"
+  },
+  {
+    "revision": "9f720a55ebc8ddf98c2593757aaa959c",
+    "url": "/404NotFound/static/media/Scarecrow.9f720a55.png"
+  }
+]);
