bc1610c864cd5aee09339e2d57eab3276479a603
villalva03 <villalva03@gmail.com>
2020-09-16 18:06:40 -0300

    firts commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+import scarecrow from './Scarecrow.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="AppContainer">
+      <header className="NotFound">
+        404 NOT FOUND
       </header>
+
+      <div className="containerError">
+        <div className="Scarecrow">
+          <img src={scarecrow} className="img" alt="scarecrow" />
+        </div>
+        
+        <div className="containerButton">
+          <div className="Ihave">
+            I have bad news for you
+          </div>
+
+          <div className="Thepage">
+            The page you are looking for might be removed or is temporally unavailable  
+          </div>
+
+          <div className="Group103">
+            <button type="button" className="Rectangle71 btn">
+              <span className="BackTOHome">BACK TO HOMEPAGE</span>
+            </button>
+          </div>
+        </div>
+      
+      </div>
+
+      <footer className="footer">
+        Villalva Francisco @ DevChallenges.io
+      </footer>
+        
     </div>
   );
 }
